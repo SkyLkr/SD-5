56784d5ff8ec290840a58be3aa39e9293419e8e0
Adriano Rocha <adriano.olr@gmail.com>
2019-09-22 12:25:29 -0300

    Correções no Middleware

--- Middleware/src/Main.java
+++ Middleware/src/Main.java
@@ -1,14 +1,23 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
 
 public class Main {
-
 	public static void main(String[] args) {
 		try {
+			
 			URL url = new URL("http://localhost:8080/Server/facerec");
 			
-			Scanner in = new Scanner(url.openStream());
+			URLConnection connection = url.openConnection();
+			long before = System.currentTimeMillis();
+			connection.connect();
+			long after = System.currentTimeMillis();
+			
+			System.out.println("Latency: " + (after-before) + "ms");
+			
+			Scanner in = new Scanner(connection.getInputStream());
 			
 			String json = in.nextLine();
 			
@@ -19,5 +28,4 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-
 }
