831a5781b28f6a149346141f995af2a95b74fac3
TiaMyllena <myllenah2o@gmail.com>
2019-09-26 12:27:57 -0300

    Segunda tela do android

--- Android/app/src/main/java/com/apm/sd5android/MainActivity.java
+++ Android/app/src/main/java/com/apm/sd5android/MainActivity.java
@@ -1,14 +1,82 @@
 package com.apm.sd5android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    Uri file;
+    private ImageView imageView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button2);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            button.setEnabled(false);
+            ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE }, 0);
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                File f = getOutputMediaFile();
+                file = Uri.fromFile(f);
+                Log.d("Image", file.toString());
+
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, file);
+
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    private File getOutputMediaFile() {
+        File mediastorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "CameraDemo");
+
+        if (!mediastorageDir.exists()) {
+            if (!mediastorageDir.mkdirs()) {
+                Log.d("CameraDemo", "failed to create directory");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format((new Date()));
+        return new File(mediastorageDir.getPath()+File.separator + "IMG_"
+                +timeStamp + ".jpg");
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 100){
+            if (resultCode == RESULT_OK){
+                imageView.setImageURI((file));
+
+            }
+        }
     }
 }
